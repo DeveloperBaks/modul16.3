b891ad3dcc751b6a592b7b5a3a71ad846cba550e
shadon2024 <in-con23@yandex.ru>
2024-04-20 15:00:44 +0500

    nameStudent

--- mod_16/mod_16/ViewController.swift
+++ mod_16/mod_16/ViewController.swift
@@ -9,12 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var name  = "Shodon Rizoev"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         // name
-        // surname
-        // lasname
+        
     }
 
 
